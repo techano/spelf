4db04e84310260f9285bd257a7a8b75483f60ed7
techano <20607160+techano@users.noreply.github.com>
2022-02-03 09:27:15 -0700

    corrected signout fucntion name

--- assets/scripts/gsignin.js
+++ assets/scripts/gsignin.js
@@ -1,11 +1,11 @@
-alert("G Sign in script v2 loaded");
+alert("G Sign in script v2.1 loaded");
 
 function onSignIn(googleuser) {
   var profile = auth2.currentUser.get().getBasicProfile();
   alert("successful login,");
 }
 
-function signOut() {
+function SignOut() {
     var auth2 = gapi.auth2.getAuthInstance();
           auth2.signOut().then(function () {
           alert("User signed out.");
