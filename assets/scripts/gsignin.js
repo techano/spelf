22e582253ecaeddcc167a22d1adc3df298954d21
techano <20607160+techano@users.noreply.github.com>
2022-02-03 09:21:59 -0700

    added sign out function

--- assets/scripts/gsignin.js
+++ assets/scripts/gsignin.js
@@ -4,3 +4,10 @@ function onSignIn(googleuser) {
   var profile = auth2.currentUser.get().getBasicProfile();
   alert("successful login,");
 }
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+          auth2.signOut().then(function () {
+          alert("User signed out.");
+          });
+}
